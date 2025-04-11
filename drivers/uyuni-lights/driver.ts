00d9f43e45aafc74622ca4461bdac32ef2249cc9
philipostli <55027973+philipostli@users.noreply.github.com>
2025-04-11 23:08:05 +0200

    Add as many remotes as you like

--- drivers/uyuni-lights/driver.ts
+++ drivers/uyuni-lights/driver.ts
@@ -13,27 +13,39 @@ module.exports = class UyuniRemoteDriver extends RFDriver {
     this.log('UyuniRemoteDriver has been initialized');
   }
 
-
   async onPair(session: PairSession) {
     session.setHandler('list_devices', this.listDevices.bind(this));
   }
-
-
 
   /**
    * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
    * This should return an array with the data of devices that are available for pairing.
    */
   async listDevices(data: any): Promise<any> {
-    return [
-      // Example device data, note that `store` is optional
-      {
-        name: 'Uyuni Remote',
-        data: {
-          id: 'uyuni-remote',
-        },
-      },
-    ];
-  }
+    try {
+      // Get all existing devices
+      const existingDevices = this.getDevices();
+      
+      // Find the next available number for uyuni-remote-N
+      const existingIds = existingDevices.map((device: any) => device.getData().id);
+      let nextNumber = 1;
+      while (existingIds.includes(`uyuni-remote-${nextNumber}`)) {
+        nextNumber++;
+      }
 
+      // Return a new device with the next available number
+      return [{
+        name: `Uyuni Remote ${nextNumber}`,
+        data: {
+          id: `uyuni-remote-${nextNumber}`
+        },
+        settings: {
+          satellite_mode_antenna: 'self'
+        }
+      }];
+    } catch (error) {
+      this.error('Error getting available devices:', error);
+      return [];
+    }
+  }
 };
